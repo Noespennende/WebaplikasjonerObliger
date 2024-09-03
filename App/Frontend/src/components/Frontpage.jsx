d3a18f9ff1700887e8676931297d9251b1ff1945
Andreasberg89 <andber9@gmail.com>
2024-09-03 16:50:22 +0200

    Use effect i Frontpage kjører nå abort på fetch funksjon ved opprydning

--- App/Frontend/src/components/Frontpage.jsx
+++ App/Frontend/src/components/Frontpage.jsx
@@ -16,6 +16,7 @@ export default function Frontpage(){
 
     useEffect(() => {
         fetchJsonDataFromServer()
+        return() => fetchJsonDataFromServer.abort();
     },[])
 
     useEffect(() => {
