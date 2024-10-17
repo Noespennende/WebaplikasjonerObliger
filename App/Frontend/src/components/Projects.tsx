50cc1f133f1831f6dd63960a9e28a94f9c6e3b06
Andreasberg89 <andber9@gmail.com>
2024-10-17 17:46:54 +0200

    feat: Front page now animates the projectcardsection dynamicly based on viewport height

--- App/Frontend/src/components/Projects.tsx
+++ App/Frontend/src/components/Projects.tsx
@@ -5,8 +5,50 @@ import LoadingProjectCard from "./LoadingProjectCard"
 
 export default function Projects (){
 
-    const [articles, setArticles] = useState()
+    const [currentPageWidth, setCurrentPageWidth] = useState(window.innerWidth)
+    const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(false)
+    const [loadingCardAmount, setLoadingCardAmount] = useState(3)
+    const [projectCardsSectionHeight, setProjectCardsSectionHeight] = useState(200)
+
+    const projectCardHeight = 350
+    const projectCardWith = 300
+    const projectCardMargin = 40
+    
+    //updates browser window size
+    const handleWindowResize = () => {
+        setCurrentPageWidth(window.innerWidth)
+    }
+
+    const calculateProjectCardsSectionHeight = () => {
+        let cardsInEachRow: number
+        let totalRowMargin: number
+
+        if (currentPageWidth >= (projectCardWith * 3 + 2* projectCardMargin)){
+            cardsInEachRow = 3
+        } 
+        else if (currentPageWidth >=  (projectCardWith * 2 + projectCardMargin)){
+            cardsInEachRow = 2
+        } else {
+            cardsInEachRow = 1
+        }
+
+        if (loading){
+            const projectCardRows = Math.ceil(loadingCardAmount/cardsInEachRow)
+            totalRowMargin = projectCardRows * projectCardMargin
+            return(
+                projectCardHeight*projectCardRows + totalRowMargin
+            )
+
+        }
+
+        const projectCardRows = Math.ceil(articles.length/cardsInEachRow)
+        totalRowMargin = Math.ceil(projectCardRows * projectCardMargin)
+
+        return (
+            projectCardRows * projectCardHeight + totalRowMargin
+        )
+    }
     
     const fetchJsonDataFromServer = async () => {
         setLoading(true)
@@ -16,7 +58,6 @@ export default function Projects (){
         .catch((error) => console.error("Data could not be found", error))
         .finally(() => setLoading(false))
     }
-
 
     const generateLoadingCards = (amount : number) => {
         return(
@@ -30,18 +71,44 @@ export default function Projects (){
 
     }
 
+    const generateHtmlClassWhenLoaded = () => {
+        const projectCardsSection = document.getElementById('projectCards')
+
+        if (!loading && projectCardsSection) {
+            projectCardsSection.classList.add('loaded')
+            setProjectCardsSectionHeight(200)
+        } 
+        else if (loading && projectCardsSection) {
+            projectCardsSection.classList.remove('loaded')
+            setProjectCardsSectionHeight(500)
+        }
+    }
 
     useEffect(() => {
+        //Listen to window resize and updates variables with new size
+        window.addEventListener('resize', handleWindowResize)
+
+        //Fetch projects from server
         const controller = new AbortController()
         fetchJsonDataFromServer()
         return() => controller.abort()
     },[])
 
     useEffect(() => {
+        generateHtmlClassWhenLoaded()
+    },[loading])
+
+    useEffect(() => {
     },[articles])
 
     return (
-    <section id="projectCards">
+    <section 
+        id="projectCards"
+        //animate height based on content
+        style={{
+            height: `${typeof projectCardsSectionHeight === 'number' ? `${calculateProjectCardsSectionHeight()}px` : "fit-content"}`
+    }}
+    >
             <ul>
                 {!loading ? 
                 (
@@ -59,7 +126,7 @@ export default function Projects (){
                                 ))
                         : <li id="noProsjects">Ingen prosjekter 😞</li>    
                     )
-                    : generateLoadingCards(6)
+                    : generateLoadingCards(loadingCardAmount)
         
                 }
                 
